028c1caac6e9b2332d3e45dfe9ad680f4702c32b
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-11 11:05:32 +0700

    Add some routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['ws/test'] = 'webservice/Test/index';
 $route['ws/auth/login'] = 'webservice/Wsauth/login';
+$route['ws/content'] = 'webservice/Wscontent/index';
+$route['ws/content/add'] = 'webservice/Wscontent/add';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
