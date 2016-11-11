7003869d623b53f75f0998bccf91bf704c032b43
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-11 07:54:45 +0700

    Add Webservice support

--- application/controllers/webservice/Test.php
+++ application/controllers/webservice/Test.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * This <lide-app.com.io> project created by : 
- * Name         : Muhammad Syafiq
- * Date / Time  : 10 November 2016, 4:31 PM.
+ * Name         : syafiq
+ * Date / Time  : 10 November 2016, 8:43 PM.
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welkom extends CI_Controller
+class Test extends CI_Controller
 {
     /**
      * Index Page for this controller.
@@ -34,6 +34,21 @@ class Welkom extends CI_Controller
 
     public function index()
     {
-        echo "abc";
+        log_message('ERROR', var_export($_POST, true));
+        log_message('ERROR', var_export($_GET, true));
+        log_message('ERROR', var_export($_REQUEST, true));
+        if ($this->input->is_ajax_request())
+        {
+            log_message('ERROR', "is_ajax");
+            header('Content-type: application/json');
+            echo json_encode(array('value' => 200));
+        }
+        else
+        {
+            log_message('ERROR', "is_not_ajax");
+            header('Content-type: application/json');
+            echo json_encode(array('value' => 404));
+        }
+
     }
 }
